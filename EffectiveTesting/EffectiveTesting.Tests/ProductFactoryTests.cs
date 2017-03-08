2a682641dc6364e6e1f5d6c5d5c51ba7603b8362
Jiri Pokorny <jiri.pokorny@solarwinds.com>
2017-03-08 13:58:25 +0100

    Aded exmple tests for ProductsFactory

---
+++ EffectiveTesting/EffectiveTesting.Tests/ProductFactoryTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace EffectiveTesting.Tests
+{
+    [TestFixture]
+    public class ProductFactoryTests
+    {
+        [Test]
+        public void TestCreate()
+        {
+            var factory = new ProductsFactory();
+            Product created = factory.Create("ExpectedName");
+            Assert.AreEqual("ExpectedName", created.Name);
+            Assert.IsTrue(DateTime.Now.AddSeconds(-1) < created.Created);
+        }
+    }
+}
